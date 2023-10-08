86384055c1e2b954abee49090a3457e4472579b2
Kiana Heidari <kiana_hedari71@yahoo.com>
2023-10-08 00:16:07 -0400

    firstGitcommit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+
+
+
+const tick = ()=>{
+  const elem = (
+    <div>
+      <h1>
+        Hi friends
+      </h1>
+      <h2>it is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  createRoot(document.getElementById('root')).render(elem);
+}
+
+setInterval(()=>{
+  tick();
+},1000)
+
+
+
